0a33f8b5cb5d72e16f7cb2506784d3b8c31429b5
T.Kato <90743387+T-Kato-github@users.noreply.github.com>
2022-08-11 16:43:26 +0900

    Add files via upload

--- COBS.cpp
+++ COBS.cpp
@@ -2,23 +2,26 @@
 #include <stdint.h>
 #include <cmath>
 #include <iostream>
-COBS::COBS(int data_size,int point_digit)
+
+template <class T>
+COBS<T>::COBS(int data_size,int point_digit)
 {
     this->point_digit = point_digit;
     this->data_size = data_size;
 }
-void COBS::encode(float const* data,int data_length,uint8_t *encoded_data)
+template <class T>
+void COBS<T>::encode(float const* data,int data_length,uint8_t *encoded_data)
 {
     int cobs_data_length = this->data_size*data_length+2;
     uint8_t *cobs_data = new uint8_t[cobs_data_length]();
-    int16_t *int_data = new int16_t[data_length]();
+    T *int_data = new T[data_length]();
     int i,j,index;
     float inflation_rate = std::pow(10.0,this->point_digit);
     //convert float data to int data.
     for(i=0;i<data_length;++i) int_data[i] = data[i]*inflation_rate;
     //Split a int16_t datum into two uint8_t data.
     for(i=0;i<data_length;++i){
-        for(j=0;j<this->data_size;++j)cobs_data[2*i+j+1] = int_data[i]>>8*j;
+        for(j=0;j<this->data_size;++j)cobs_data[this->data_size*i+j+1] = int_data[i]>>8*j;
     }
     //count zero position.
     j=1;
@@ -37,12 +40,13 @@ void COBS::encode(float const* data,int data_length,uint8_t *encoded_data)
     delete[] cobs_data;
     delete[] int_data;
 }
-void COBS::decode(uint8_t const* data,int data_length,float *decoded_data)
+template <class T>
+void COBS<T>::decode(uint8_t const* data,int data_length,float *decoded_data)
 {
     int zero_position;
     int data_num = int((data_length-2)/this->data_size);
     uint8_t *_data = new uint8_t[data_length]();
-    int16_t *_decoded_data = new int16_t[data_num]();
+    T *_decoded_data = new T[data_num]();
     int i,j;
     float inflated_rate = std::pow(10.0,-this->point_digit);
     //deep_copy data to _data
@@ -55,7 +59,7 @@ void COBS::decode(uint8_t const* data,int data_length,float *decoded_data)
     {
         for(j=0;j<this->data_size;++j)
         {
-            _decoded_data[i] = _decoded_data[i] |  (_data[2*i+j+1]<<(8*j) );
+            _decoded_data[i] = _decoded_data[i] |  (_data[this->data_size*i+j+1]<<(8*j) );
         }
     }
     //deep_copy _decoded_data to decoded_data
@@ -64,3 +68,12 @@ void COBS::decode(uint8_t const* data,int data_length,float *decoded_data)
     delete[] _data;
     delete[] _decoded_data;
 }
+template <class T>
+int COBS<T>::get_data_size()
+{
+    return this->data_size;
+}
+
+template class COBS<int8_t>;
+template class COBS<int16_t>;
+template class COBS<int32_t>;
--- COBS.hpp
+++ COBS.hpp
@@ -12,9 +12,14 @@
 #include <stddef.h>
 #include <stdint.h>
 /**
- * @brief 
- * Class for encoding or decoding based on COBS.
+ * @brief Class for encoding or decoding based on COBS.  
+ * 
+ * @tparam T 
+ * When data_size is 2, T shuold be int16_t. 
+ * When data_size is 4, T shuold be int32_t.
+ * When data_size is 5, T should be int64_t.
  */
+template <class T>
 class COBS{
     private:
         int point_digit;
@@ -43,5 +48,11 @@ class COBS{
          * @param decoded_data Array for decoding results. 
          */
         void decode(uint8_t const* data,int data_length,float *decoded_data);
+        /**
+         * @brief Get the data size object
+         * 
+         * @return int 
+         */
+        int get_data_size();
 };
     
