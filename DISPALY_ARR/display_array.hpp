dfdf7010e5f29f422b98f450d965c558ff10c060
T.Kato <90743387+T-Kato-github@users.noreply.github.com>
2022-08-11 16:58:29 +0900

    Create display_array.hpp

---
+++ DISPALY_ARR/display_array.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+#include "stdint.h"
+void display_array(uint8_t const* arr,int len);
+void display_array(char const* arr,int len);
+void display_array(int16_t const* arr,int len);
+void display_array(int const* arr,int len);
+void display_array(float const* arr,int len);
